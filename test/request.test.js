2abb6c86ead49306916fc9feb43fd728d8cbb934
Aleksey Tsvetkov <acvetkov@yandex-team.ru>
2015-07-27 16:37:59 +0300

    remove sinon dependency

--- test/request.test.js
+++ test/request.test.js
@@ -1,7 +1,6 @@
 describe('request', function() {
 
     var assert = require('chai').assert;
-    var sinon = require('sinon');
     var Request = require('../src/request');
 
     'use strict';
@@ -13,14 +12,10 @@ describe('request', function() {
         });
 
         it('should call handler', function () {
-            var spy = sinon.spy();
-            Request.onCreate(spy);
-
-            new Request();
-            new Request();
-            new Request();
-
-            assert.ok(spy.calledThrice);
+            Request.onCreate(function (req) {
+                assert.instanceOf(req, Request);
+            });
+            var req = new Request();
         });
     });
 
@@ -31,13 +26,11 @@ describe('request', function() {
         });
 
         it('should call handler', function () {
-            var spy = sinon.spy();
-            Request.onSend(spy);
-
-            new Request().send();
-            new Request().send();
-
-            assert.ok(spy.calledTwice);
+            Request.onSend(function (req) {
+                assert.instanceOf(req, Request);
+            });
+            var request = new Request();
+            request.send();
         });
     });
 
